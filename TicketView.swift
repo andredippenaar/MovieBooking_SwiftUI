4fed235c453b903f6c4d7679ea850dafede7e3fd
andredippenaar <andre.pierre.dippenaar@gmail.com>
2022-12-02 14:09:35 +0200

    Create basic TicketView with animation

--- TicketView.swift
+++ TicketView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct TicketView: View {
+    @State var animate = false
+    
     var body: some View {
         ZStack {
+            CircleBackground(color: Color("greenCircle"))
+                .blur(radius: animate ? 30 : 100)
+                .offset(x: animate ? -50 : -130, y: animate ? -30 : -100)
+                .task {
+                    withAnimation(.easeInOut(duration: 7).repeatForever()) {
+                        animate.toggle()
+                    }
+                }
+            
+            CircleBackground(color: Color("pinkCircle"))
+                .blur(radius: animate ? 30 : 100)
+                .offset(x: animate ? 100 : 130, y: animate ? 150 : 100)
+            
             VStack(spacing: 30.0) {
                 Text("Mobile Ticket")
                     .font(.title3)
